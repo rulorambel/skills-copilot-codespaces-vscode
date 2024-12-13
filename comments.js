cc41144f5c793753939aa04e8bff4438c9bffd27
RuloRamBel <61566150+rulorambel@users.noreply.github.com>
2024-12-13 23:19:20 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,11 @@
+// Create web server 
+const express = require('express');
+const app = express();
+const port = process.env.PORT || port;
+const www = process.env.WWW || './';
+app.use(express.static(www));
+console.log(`serving ${www}`);
+app.get('*', (req, res) => {
+    res.sendFile(`index.html`, { root: www });
+});
+app.listen(port, () => console.log(`listening on http://localhost:${port}`)
